f8068a3b2d286fc1de098d43f2783ae6975eeacb
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-17 20:20:03 -0300

    Add 'new book' form to website structure

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myBooks = [];
 
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,5 +9,14 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+
+let book1 = new Book('Teste título', 'Israel Carvalho', 350, 'yes');
+let book2 = new Book('Teste título2', 'Israel Carvalho2', 20, 'no');
+let book3 = new Book('Teste título3', 'Israel Carvalho3', 3502, 'yes');
+
+myBooks.push(book1);
+myBooks.push(book2);
+myBooks.push(book3);
