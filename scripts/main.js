be12702de93d87406ad072608650f27406617a8c
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-18 11:51:42 -0300

    Reorder file structure, add more functions, modal
    This commit reorder file structure to be more concise and organized.
    Add temporary function to insert dummy books.
    Add modal initial structure.

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+const myBooks = [];
+
+const addNewBookModal = document.querySelector("#addNewBookModal");
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooks() {
+    const bookList = document.querySelector("#bookList");
+
+    for (book in myBooks) {
+        const bookDiv = document.createElement("div");
+        bookDiv.setAttribute("class", (`book-${book}`));
+
+        const elements = ['bookTitle', 'bookAuthor', 'bookPages', 'bookRead'].map(() => document.createElement("p"));
+
+        elements[0].textContent = 'Book Title: ' + myBooks[book].title;
+        bookDiv.append(elements[0]);
+        elements[1].textContent = 'Book Author: ' + myBooks[book].author;
+        bookDiv.append(elements[1]);
+        elements[2].textContent = 'Book Pages: ' + myBooks[book].pages;
+        bookDiv.append(elements[2]);
+        elements[3].textContent = 'Book read? ' + myBooks[book].read;
+        bookDiv.append(elements[3]);
+
+        bookList.append(bookDiv);
+    }
+}
+
+
+
+///////
+function tempBooks() {
+    let book1 = new Book('Teste título', 'Israel Carvalho', 350, 'yes');
+    let book2 = new Book('Teste título2', 'Israel Carvalho2', 20, 'no');
+    let book3 = new Book('Teste título3', 'Israel Carvalho3', 3502, 'yes');
+
+    myBooks.push(book1);
+    myBooks.push(book2);
+    myBooks.push(book3);
+
+}
+
+// Calls
+tempBooks();
+
+
+displayBooks();
